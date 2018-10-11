a2824d1fb60e5e9448d57cc347868a4e3da30f2d
tianmeng <tianmeng1@jd.com>
2018-10-11 17:17:54 +0800

    feat: 支持postcss

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,9 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {
+      "browsers": ['last 5 versions','Android >= 4.0','iOS >= 6'],
+    },
+    "precss": {},
   }
 }
