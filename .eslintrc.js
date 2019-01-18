372f76a499bda3c1fcc9e8b802b3a4d7294bf5a7
ycshill <shilili1992@gmail.com>
2019-01-18 16:37:25 +0800

    feat: 修改eslint.js 文件，使其能够检测html文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,11 @@ module.exports = {
   },
   // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
   // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential', 'airbnb-base'],
+  extends: [
+    // 'plugin:vue/essential',
+    'plugin:vue/recommended', // 使起能够检查 vue文件中的 html
+    'airbnb-base'
+  ],
   // required to lint *.vue files
   plugins: [
     'vue'
